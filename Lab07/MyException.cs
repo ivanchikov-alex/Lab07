0e08dae4fd031804ea83e0c7bc5cc4cb3d3886dc
ivanchikov-alex <mr.ivanchikov@yandex.by>
2020-12-05 13:05:53 +0300

    ничего нет

---
+++ Lab07/MyException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab07
+{
+    public class MyException:Exception//имя придется поменять
+    {
+        public MyException()
+        {
+            /*значения свойств по умолчанию*/
+        }
+        /*будут свойства*/
+    }
+
+    public class MyException2 : MyException//имя придется поменять
+    {
+        public MyException2()
+        {
+            /*значения свойств по умолчанию*/
+        }
+        /*будут свойства*/
+    }
+
+    public class MyException3 : MyException2//имя придется поменять
+    {
+        public MyException3()
+        {
+            /*значения свойств по умолчанию*/
+        }
+        /*будут свойства*/
+    }
+}
